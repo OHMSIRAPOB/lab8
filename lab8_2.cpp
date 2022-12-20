845d3e08a747b54b4a89d2ca66f3558330a4d611
ohmsirapob <fedfekfc@gmail.com>
2022-12-20 16:39:23 +0700

    lab8_3copleted

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -11,7 +11,7 @@ int main (){
     getline(cin,name);
     cout << "Fahsai: Wow!!! " << name << " is a really cool name."<<endl;
     cout << "Fahsai: I think you are an Engineering student. What is your student ID?"<<endl;
-    cout << name <<":";
+    cout << name <<": ";
 
     unsigned long int id;
 
@@ -20,7 +20,7 @@ int main (){
     cout << "Fahsai: I think you may be GEAR " << (id/10000000)-12 <<". I have a free movie ticket for you."<<endl;
     cout << "Fahsai: Let's go to the cinema together!!!"<<endl;
     cout << "Fahsai: What movie do you want to watch?"<<endl;
-    cout << name <<":";
+    cout << name <<": ";
     getline(cin,movie);
     cout << "Fahsai: So....which day are you free to go with me?"<<endl;
     cout << name <<": ";
